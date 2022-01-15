ed54e55f63991859488be531b4a0c06b70183650
kgrundemann <krzgrund@gmail.com>
2022-01-15 12:01:51 +0100

    first commit

---
+++ myScript.js
@@ -0,0 +1,21 @@
+"use strict"
+
+function addRow() {
+  var marka = document.getElementById("marka").value;
+  var model = document.getElementById("model").value;
+  var kolor = document.getElementById("kolor").value;
+  var table = document.getElementsByTagName("table")[0];
+  var newRow = table.insertRow(table.rows.length);
+
+  var cel1 = newRow.insertCell(0);
+  var cel2 = newRow.insertCell(1);
+  var cel3 = newRow.insertCell(2);
+
+  cel1.innerHTML = marka;
+  cel2.innerHTML = model;
+  cel3.innerHTML = kolor;
+}
+
+function onClick() {
+  addRow();
+}
