f3c6addf1ca83dc0adf22e4c2298414ca692c4a9
kgrundemann <krzgrund@gmail.com>
2022-01-19 21:16:18 +0100

    input tworzy obiekt car

--- myScript.js
+++ myScript.js
@@ -1,9 +1,9 @@
 "use strict"
-
+let cars=[];
 function addRow() {
-  var marka = document.getElementById("marka").value;
-  var model = document.getElementById("model").value;
-  var kolor = document.getElementById("kolor").value;
+  var marka = document.getElementById('marka').value;
+  var model = document.getElementById('model').value;
+  var kolor = document.getElementById('kolor').value;
   var table = document.getElementsByTagName("table")[0];
   var newRow = table.insertRow(table.rows.length);
 
@@ -15,7 +15,18 @@ function addRow() {
   cel2.innerHTML = model;
   cel3.innerHTML = kolor;
 }
+function addCar() {
+  let car = {
+    marka: document.getElementById('marka').value,
+    model: document.getElementById('model').value,
+    kolor: document.getElementById('kolor').value,
+  }
+  cars.push(car);
+  document.forms[0].reset(); //resetowanie formularza
+  console.log(cars)
+}
 
 function onClick() {
   addRow();
+  addCar();
 }
