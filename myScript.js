01f206876ddea00073d505e272c29e665c01deed
kgrundemann <krzgrund@gmail.com>
2022-01-20 23:12:56 +0100

    funkcja walidacji

--- myScript.js
+++ myScript.js
@@ -1,36 +1,56 @@
-"use strict"
-let cars=[];
-function addRow() {
-  var marka = document.getElementById('marka').value;
-  var model = document.getElementById('model').value;
-  var kolor = document.getElementById('kolor').value;
-  var table = document.getElementsByTagName("table")[0];
-  var newRow = table.insertRow(table.rows.length);
+"use strict";
+let cars = [];
 
-  var cel1 = newRow.insertCell(0);
-  var cel2 = newRow.insertCell(1);
-  var cel3 = newRow.insertCell(2);
+function validate(newCar) {
+  let success = true;
+	if (newCar.marka === "") {
+		alert("Marka nie może być pusta");
+		success =false;
+	}
+	else if (newCar.model === "") {
+		alert("Model nie może być pusty");
+		success =false;
+	}
+	else if (newCar.kolor === "") {
+		alert("Kolor nie może być pusty");
+		success =false;
+	}
+	return success;
+}
 
-  cel1.innerHTML = marka;
-  cel2.innerHTML = model;
-  cel3.innerHTML = kolor;
+function addRow(newCar) {
+	let table = document.getElementsByTagName("table")[0];
+	var newRow = table.insertRow(table.rows.length);
+
+	var cel1 = newRow.insertCell(0);
+	var cel2 = newRow.insertCell(1);
+	var cel3 = newRow.insertCell(2);
+
+	cel1.innerHTML = newCar.marka;
+	cel2.innerHTML = newCar.model;
+	cel3.innerHTML = newCar.kolor;
 }
-function addCar() { 
-  var table= document.querySelector("table");
-  var car = {
-    marka: document.getElementById('marka').value,
-    model: document.getElementById('model').value,
-    kolor: document.getElementById('kolor').value,
-  }
-  cars.push(car);
-  document.forms[0].reset(); //resetowanie formularza
-  console.log(cars)
-  
-  if (!cars.length<=0){
-    table.style.visibility = "visible";
-  }
+
+function addCar(newCar) {
+	cars.push(newCar);
+	document.forms[0].reset(); //resetowanie formularza
 }
+
+function showTable() {
+	let table = document.querySelector("table");
+	if (cars.length > 0) table.style.visibility = "visible";
+}
+
 function onClick() {
-  addRow();
-  addCar();
+	const newCar = {
+		marka: document.getElementById("marka").value,
+		model: document.getElementById("model").value,
+		kolor: document.getElementById("kolor").value,
+	};
+
+	if (validate(newCar)) {
+		addRow(newCar);
+		addCar(newCar);
+		showTable();
+	}
 }
